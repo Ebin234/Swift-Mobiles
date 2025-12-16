7de631acdabd4de2122b090f643d786bbd5f4d5e
Ebin234 <ebinyesudas234@gmail.com>
2025-12-16 22:02:38 +0530

    updated deleteProduct server action

--- src/lib/actions/deleteProduct.action.ts
+++ src/lib/actions/deleteProduct.action.ts
@@ -2,12 +2,15 @@
 
 import Product from "@/models/product";
 import { connectDB } from "../mongodb";
+import cloudinary from "../cloudinary";
 
 export const deleteProduct = async (id: string) => {
   console.log({ id });
   try {
     await connectDB();
     const data = await Product.findByIdAndDelete(id);
+    console.log("delete",data)
+    await cloudinary.uploader.destroy(data.imagePublicId);
     return { success: true };
   } catch (e) {
     console.error(e);
