2ae753b5628294be51f89a374e4fc4567be1a1c4
Ebin234 <ebinyesudas234@gmail.com>
2025-12-28 22:18:25 +0530

    edit product bug resolved

--- src/lib/actions/fetchProductById.action.ts
+++ src/lib/actions/fetchProductById.action.ts
@@ -1,15 +1,18 @@
 import Product from "@/models/product";
+import { connectDB } from "../mongodb";
 
-
-export const fetchProductById = async (id:string)=>{
-    try{
-        console.log("fetchProduct",id)
-        const product = await Product.findById(id).lean();
-        console.log("fetch",product)
-        const jsonData = JSON.parse(JSON.stringify(product))
-        console.log({jsonData})
-        return {success:true,jsonData};
-    }catch(error){
-        return {success:false,e:error};
-    }
-}
+export const fetchProductById = async (id: string) => {
+  try {
+    await connectDB();
+    console.log("fetchProduct", id);
+    const product = await Product.findById(id).lean();
+    console.log("fetch", product);
+    const jsonData = JSON.parse(JSON.stringify(product));
+    console.log("jsondata", jsonData);
+    console.log({ jsonData });
+    return { success: true, jsonData };
+  } catch (error) {
+    console.log({ error });
+    return { success: false, e: error };
+  }
+};
